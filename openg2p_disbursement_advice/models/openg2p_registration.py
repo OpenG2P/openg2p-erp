216ae31b92c6988274143339addfc4fa8df0b2b0
Rohnak Agarwal <rrka79wal@gmail.com>
2021-05-27 13:05:15 +0530

    bank fields in registration

--- openg2p_disbursement_advice/models/openg2p_registration.py
+++ openg2p_disbursement_advice/models/openg2p_registration.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2020 OpenG2P (https://openg2p.org)
-# @author: Salton Massally <saltonmassally@gmail.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
