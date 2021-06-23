b35858250b9e290e4ea5c4b38d079a1516e1b8b5
SubhamPramanik <subhampramanik@gmail.com>
2021-06-23 21:51:39 +0530

    [Module] ODK Connector (#10)
    * ODK Connector update
    
    * No submodule

---
+++ odk-connector/models/odk.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, exceptions
+
+from requests.auth import AuthBase
+import requests
+import json
+import logging
+
+_logger = logging.getLogger(__name__)
+
+# Retry mechanism that needs to be decided
+_MAX_NUMBER_REQUEST = 30
+
+# _BASE_URL = 'https://odk.tekkie.codes/'
+
+_ODK_TYPE_URL = {
+    'auth': {
+        'session': 'v1/sessions',
+    },
+    'submission': {
+        'odata': 'v1/projects/%s/forms/%s.svc/Submissions',
+    },
+}
+
+_CODE_401 = 401
+_CODE_403 = 403
+_CODE_422 = 422
+_CODE_200 = 200
+_CODE_201 = 201
+
+
+class ODK(object):
+
+    def __init__(self, base_url, operation_type, user, password, max_try=_MAX_NUMBER_REQUEST):
+        super(ODK, self).__init__()
+        self.base_url = base_url
+        self.user = user
+        self.operation_type = operation_type
+        self.max_try = max_try
+        self.auth = HTTPTokenAuth(base_url, user, password)
+
+    # Build URL for the API call.
+    # TODO: Add support for updating without calling stored data
+    def _build_url(self, arguments, url_type='odata'):
+        arguments = arguments and arguments or {}
+        url = _ODK_TYPE_URL[self.operation_type][url_type]
+        if self.operation_type not in _ODK_TYPE_URL.keys():
+            raise exceptions.Warning(
+                _("'%s' is not implemented.") % self.operation_type
+            )
+        complete_url = self.base_url + url % tuple(arguments)
+        return complete_url
+
+    # Caller function for GET method
+    def get(self, arguments, params, url_type='odata'):
+        url = self._build_url(arguments, url_type)
+        return self.call_api(url, params, 'get')
+
+    # Function to make API calls
+    def call_api(self, url, query_params, call_type, data=False):
+        _logger.info("Calling %s" % url)
+        query_params = query_params and query_params or {}
+
+        for i in range(self.max_try):
+            try:
+                if call_type == "get":
+                    response = requests.get(url, params=query_params, auth=self.auth)
+                    break
+                elif call_type == "post":
+                    json_data = json.dumps(data)
+                    response = requests.post(url, params=query_params, auth=self.auth, json=json_data)
+                    break
+            except Exception as err:
+                _logger.warning(
+                    "URL Call Error. %d/%d. URL: %s",
+                    i, self.max_try, err.__str__(),
+                )
+        else:
+            raise exceptions.Warning(_('Maximum attempts reached.'))
+
+        if response.status_code == _CODE_401:
+            raise exceptions.Warning(_(
+                "401 - Unable to authenticate to ODK with the user '%s'.\n") % self.user)
+        elif response.status_code not in [_CODE_200, _CODE_201]:
+            raise exceptions.Warning(
+                _("The call to '%s' failed:\n"
+                  "- Status Code: %d\n"
+                  "- Reason: %s\n"
+                  "- Body: %s") % (
+                    response.url, response.status_code, response.reason, response.text))
+        return response.json()
+
+
+# Class to handle authentication
+class HTTPTokenAuth(AuthBase):
+
+    auth_header_format = "Bearer {}"
+
+    def __init__(self, base_url, user, password):
+        auth_data = {
+            'email': user,
+            'password': password
+        }
+        url = base_url + _ODK_TYPE_URL['auth']['session']
+        response_data = self.auth_call(url, auth_data)
+        self.token = response_data['token']
+
+    def __call__(self, request):
+        request.headers["Authorization"] = self.auth_header_format.format(self.token)
+        return request
+
+    # Call ODK API and get token for user
+    def auth_call(self, url, data):
+        try:
+            response = requests.post(url, json=data)
+        except Exception as err:
+            raise exceptions.Warning(_("Error in calling Auth URL: '%s'.\n") % url)
+
+        if response.status_code == _CODE_401:
+            raise exceptions.Warning(_(
+                "401 - Unable to authenticate to ODK with the user '%s'.\n") % self.user)
+        elif response.status_code not in [_CODE_200, _CODE_201]:
+            raise exceptions.Warning(
+                _("The call to '%s' failed:\n"
+                  "- Status Code: %d\n"
+                  "- Reason: %s\n"
+                  "- Body: %s") % (
+                    response.url, response.status_code, response.reason, response.text))
+        return response.json()
---
+++ odk-connector/models/odk_config_model.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+from .odk import ODK
+
+
+class ODKConfig(models.Model):
+    _name = 'odk.config'
+    _description = 'ODK Form Configuration'
+    _order = 'id'
+
+    # Columns
+    form_name = fields.Char(
+        string="Form Name",
+        required=True,
+    )
+    odk_endpoint = fields.Char(
+        string='ODK Base URL',
+        required=True,
+        # readonly=True
+    )
+    odk_project_id = fields.Integer(
+        string='ODK Project ID',
+        required=True,
+        # readonly=True
+    )
+    odk_form_id = fields.Char(
+        string='ODK Form ID',
+        required=True,
+        # readonly=True
+    )
+    odk_email = fields.Char(
+        string='ODK User EMail',
+        required=True,
+        # readonly=True
+    )
+    odk_password = fields.Char(
+        string='ODK User Password',
+        required=True,
+        # readonly=True
+    )
+    is_active = fields.Boolean(
+        string="Active",
+        default=False
+    )
+    odk_last_sync_date = fields.Datetime(
+        string='Last Sync Date with ODK',
+        readonly=True
+    )
+    odk_submissions_count = fields.Integer(
+        string='Submissions Count',
+        readonly=True,
+        default=0
+    )
+
+    @api.multi
+    def odk_button_update_form_submissions(self):
+        self.call_submission()
+
+    # Method executed by cron job to fetch submissions
+    def cron_update_all_active_forms(self):
+        configs = self.search([('is_active', '=', True)])
+        configs.odk_button_update_all_form_submissions()
+        return True
+
+    # Method called by button to fetch submissions for all forms
+    def odk_button_update_all_form_submissions(self):
+        for config in self:
+            print("Config: ", config.form_name)
+            config.call_submission()
+
+    # Method calling submissions call to fetch data
+    def call_submission(self):
+        submissions_obj = self.env['odk.submissions']
+        submissions_obj.update_submissions(self)
+        print("Call Submission ends")
+
+
+
+
+
