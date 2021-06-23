0096084134557eadeded85c7a7c4c7964e961404
Rohnak Agarwal <49589987+rrkas@users.noreply.github.com>
2021-06-23 22:37:37 +0530

    [FEAT] ODK Submission -> Registration and Organisation Fields (#5)
    * beneficiary school 4 fields added
    
    * map instead of rigid fields
    
    * map instead of rigid fields (revised)
    
    * school map complete
    
    * Submission to registration
    
    * submission to registration
    
    * map in progress
    
    * mapping complete
    
    * create_registration_from_odk in process
    
    * ODK->REGD success
    
    * ODK->REGD complete
    
    * ODK->REGD complete
    
    * filter and bulk update done
    
    * code rearrange, comment removal

---
+++ openg2p_registration/models/openg2p_registration_widget.py
@@ -0,0 +1,41 @@
+from odoo import fields, models, api
+
+
+class UpdateWizard(models.TransientModel):
+    _name = 'openg2p.registration.update_att'
+
+    val = fields.Char('New Attendance')
+
+    @api.multi
+    def update_att(self):
+        regd_obj = self.env['openg2p.registration']
+        # self.ensure_one()
+        regds = regd_obj.browse(self.env.context.get('active_ids'))
+        # print(regds)
+        # print(type(regds))
+        for regd in regds:
+            print('REGD:', regd.id)
+            att = self.env['openg2p.beneficiary.orgmap'].search(
+                ['&', ('registration', '=', regd.id),
+                 ('field_name', '=', 'total_student_in_attendance_at_the_school')]
+            )
+            if att:
+                print('ATT:', att.field_value)
+                att.field_value = int(self.val)
+                print('ATT:', att.field_value)
+            else:
+                self.env['openg2p.beneficiary.orgmap'].create({
+                    'field_name': 'total_student_in_attendance_at_the_school',
+                    'field_value': self.val,
+                    'registration': regd.id,
+                })
+        return {'type': 'ir.actions.act_window_close'}
+
+    @api.multi
+    def update_stage(self):
+        regd_obj = self.env['openg2p.registration']
+        regds = regd_obj.browse(self.env.context.get('active_ids'))
+        for regd in regds:
+            print('REGD:', regd.id)
+            regd.stage_id = 5
+        return {'type': 'ir.actions.act_window_close'}
