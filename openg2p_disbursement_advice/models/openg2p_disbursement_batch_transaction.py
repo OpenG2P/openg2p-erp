aa8975f7cf68c8f97b07974f35ba487cec0e495e
ISHAN <ishanranasingh@gmail.com>
2021-06-23 23:31:28 +0530

    indentation

--- openg2p_disbursement_advice/models/openg2p_disbursement_batch_transaction.py
+++ openg2p_disbursement_advice/models/openg2p_disbursement_batch_transaction.py
@@ -22,86 +22,81 @@ BATCH_SIZE = 500
 
 
 class BatchTransaction(models.Model):
-    _name = 'openg2p.disbursement.batch.transaction'
-    _description = 'Disbursement Batch'
-    _inherit = ['generic.mixin.no.unlink',
-                'mail.thread', 'openg2p.mixin.has_document']
-    allow_unlink_domain = [('state', '=', 'draft')]
+    _name = "openg2p.disbursement.batch.transaction"
+    _description = "Disbursement Batch"
+    _inherit = ["generic.mixin.no.unlink", "mail.thread", "openg2p.mixin.has_document"]
+    allow_unlink_domain = [("state", "=", "draft")]
 
     name = fields.Char(
         required=True,
         readonly=True,
-        states={'draft': [('readonly', False)]},
-        track_visibility='onchange'
+        states={"draft": [("readonly", False)]},
+        track_visibility="onchange",
     )
     program_id = fields.Many2one(
-        'openg2p.program',
+        "openg2p.program",
         required=True,
         readonly=True,
         index=True,
-        states={'draft': [('readonly', False)]},
-        track_visibility='onchange'
+        states={"draft": [("readonly", False)]},
+        track_visibility="onchange",
     )
     state = fields.Selection(
         [
-            ('draft', 'Drafting'),
-            ('confirm', 'Confirmed'),
-            ('pending', 'Pending'),
-            ('paymentstatus', 'Transaction Status')
+            ("draft", "Drafting"),
+            ("confirm", "Confirmed"),
+            ("pending", "Pending"),
+            ("paymentstatus", "Transaction Status"),
         ],
-        string='Status',
+        string="Status",
         index=True,
         readonly=True,
         copy=False,
-        default='draft',
-        track_visibility='onchange'
+        default="draft",
+        track_visibility="onchange",
     )
     date_start = fields.Date(
-        string='Date From',
+        string="Date From",
         required=True,
         readonly=True,
-        states={'draft': [('readonly', False)]},
-        default=lambda self: fields.Date.to_string(
-            date.today().replace(day=1)),
-        track_visibility='onchange'
+        states={"draft": [("readonly", False)]},
+        default=lambda self: fields.Date.to_string(date.today().replace(day=1)),
+        track_visibility="onchange",
     )
     date_end = fields.Date(
-        string='Date To',
+        string="Date To",
         required=True,
         readonly=True,
-        states={'draft': [('readonly', False)]},
+        states={"draft": [("readonly", False)]},
         default=lambda self: fields.Date.to_string(
-            (datetime.now() + relativedelta(months=+1, day=1, days=-1)).date()),
-        track_visibility='onchange'
+            (datetime.now() + relativedelta(months=+1, day=1, days=-1)).date()
+        ),
+        track_visibility="onchange",
     )
     company_id = fields.Many2one(
-        'res.company',
-        'Company',
+        "res.company",
+        "Company",
         required=True,
         readonly=True,
-        ondelete='restrict',
-        default=lambda self: self.env.user.company_id
+        ondelete="restrict",
+        default=lambda self: self.env.user.company_id,
     )
-    request_id = fields.Char(
-        string="UUID",
-        compute="_generate_uuid",
-        store=True
-    )
+    request_id = fields.Char(string="UUID", compute="_generate_uuid", store=True)
     transaction_status = fields.Char(
         readonly=True,
     )
 
     def action_confirm(self):
         for rec in self:
-            rec.state = 'confirm'
+            rec.state = "confirm"
 
     def action_pending(self):
         for rec in self:
-            rec.state = 'pending'
+            rec.state = "pending"
 
     def action_transaction(self):
         for rec in self:
-            rec.state = 'paymentstatus'
+            rec.state = "paymentstatus"
 
     def create_bulk_transfer(self):
         self._generate_uuid()
@@ -111,78 +106,85 @@ class BatchTransaction(models.Model):
         #     os.remove('accounts.csv')
 
         limit = 100
-        beneficiary_transactions = self.env['openg2p.disbursement.main'].search(
-            [('batch_id', '=', self.id)], limit=limit)
+        beneficiary_transactions = self.env["openg2p.disbursement.main"].search(
+            [("batch_id", "=", self.id)], limit=limit
+        )
 
         offset = 0
         while len(beneficiary_transactions) > 0:
 
-            with open('accounts.csv', 'a') as csvfile:
+            with open("accounts.csv", "a") as csvfile:
                 csvwriter = csv.writer(csvfile)
                 for rec in beneficiary_transactions:
-                    entry = [rec.acc_holder_name, rec.name, rec.amount,
-                             rec.currency_id.name, rec.payment_mode]
+                    entry = [
+                        rec.acc_holder_name,
+                        rec.name,
+                        rec.amount,
+                        rec.currency_id.name,
+                        rec.payment_mode,
+                    ]
                     # print(entry)
                     beneficiary_transaction_records = []
                     beneficiary_transaction_records.append(entry)
                     csvwriter.writerows(
-                        map(lambda x: [x], beneficiary_transaction_records))
+                        map(lambda x: [x], beneficiary_transaction_records)
+                    )
 
             offset += len(beneficiary_transactions)
 
             if len(beneficiary_transactions) < limit:
                 break
-            beneficiary_transactions = self.env['openg2p.disbursement.main'].search(
-                [('batch_id', '=', self.id)], limit=limit, offset=offset)
+            beneficiary_transactions = self.env["openg2p.disbursement.main"].search(
+                [("batch_id", "=", self.id)], limit=limit, offset=offset
+            )
 
         headers = {
-            'Content-Type': 'multipart/form-data',
+            "Content-Type": "multipart/form-data",
         }
 
         files = {
-            'data': ('accounts.csv', open('accounts.csv', 'rb')),
-            'note': (None, 'Bulk transfers'),
-            'checksum': (None, str(self.generate_hash())),
-            'request_id': (None, str(self.request_id)),
+            "data": ("accounts.csv", open("accounts.csv", "rb")),
+            "note": (None, "Bulk transfers"),
+            "checksum": (None, str(self.generate_hash())),
+            "request_id": (None, str(self.request_id)),
         }
         print(self.request_id)
-        url = 'http://15.207.23.72:5000/channel/bulk/transfer'
+        url = "http://15.207.23.72:5000/channel/bulk/transfer"
 
         try:
             response = requests.post(url, headers=headers, files=files)
-            self.transaction_status = response.json().get('status')
+            self.transaction_status = response.json().get("status")
             print(response.text)
         except requests.exceptions.RequestException as e:
             print(e)
 
     def bulk_transfer_status(self):
-        params = (('request_id', str(self.request_id)),)
+        params = (("request_id", str(self.request_id)),)
 
-        url = 'http://15.207.23.72:5000/channel/bulk/transfer'
+        url = "http://15.207.23.72:5000/channel/bulk/transfer"
 
         try:
             response = requests.get(url, params=params)
             print(response.text)
-            self.transaction_status = response.json()[0]['status']
+            self.transaction_status = response.json()[0]["status"]
             return response
         except requests.exceptions.RequestException as e:
             print(e)
 
     def all_transactions_status(self):
         try:
-            response = requests.get(
-                'https://15.207.23.72:5000/channel/transfer')
+            response = requests.get("https://15.207.23.72:5000/channel/transfer")
             return response
         except BaseException as e:
             print(e)
 
     def generate_hash(self):
         sha256 = hashlib.sha256()
-        block_size = 256*128
+        block_size = 256 * 128
 
         try:
-            with open('accounts.csv', 'rb') as f:
-                for chunk in iter(lambda: f.read(block_size), b''):
+            with open("accounts.csv", "rb") as f:
+                for chunk in iter(lambda: f.read(block_size), b""):
                     sha256.update(chunk)
             return sha256.hexdigest()
         except BaseException as e:
