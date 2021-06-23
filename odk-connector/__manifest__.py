cd39ffcbbcc94bc8a83771d06f0206a572d40e13
SubhamPramanik <subhampramanik@gmail.com>
2021-06-23 21:51:39 +0530

    [Module] ODK Connector (#10)
    * ODK Connector update
    
    * No submodule

---
+++ odk-connector/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'ODK Connector',
+    'summary': 'Connect ODK Central to Odoo',
+    'version': '12.0.2.2.2',
+    'category': 'Connector',
+    'author': 'Subham Pramanik',
+    'depends': [
+        'base',
+        'web',
+        'mail',
+    ],
+    'data': [
+        'data/ir_cron.xml',
+        'views/odk_submissions_view.xml',
+        'views/odk_config_form.xml',
+        'views/action.xml',
+        'views/menu.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+}
---
+++ odk-connector/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import odk
+from . import odk_config_model
+from . import odk_submissions_model
