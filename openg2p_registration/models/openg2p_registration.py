a32f93affc396147303ff21d762d84f08cee9e81
Rohnak Agarwal <rrka79wal@gmail.com>
2021-08-11 14:11:07 +0530

    black formattings

--- openg2p_registration/models/openg2p_registration.py
+++ openg2p_registration/models/openg2p_registration.py
@@ -25,8 +25,8 @@ class Registration(models.Model):
     def _default_stage_id(self):
         ids = (
             self.env["openg2p.registration.stage"]
-                .search([("fold", "=", False)], order="sequence asc", limit=1)
-                .ids
+            .search([("fold", "=", False)], order="sequence asc", limit=1)
+            .ids
         )
         if ids:
             return ids[0]
@@ -442,7 +442,9 @@ class Registration(models.Model):
                 "lastname": "_",
                 "street": "_",
                 "location_id": 1,
-                "city": (temp["city"] or "Freetown") if "city" in temp.keys() else "Freetown",
+                "city": (temp["city"] or "Freetown")
+                if "city" in temp.keys()
+                else "Freetown",
                 "country_id": 202,
                 "state_id": 701,
                 "gender": "male",
@@ -461,7 +463,7 @@ class Registration(models.Model):
                     "regression_and_progression",
                     "total_quality",
                     "total_equity",
-                    "grand_total",
+                    "grand_total_le",
                 ]:
                     org_data[k] = v
                     continue
@@ -664,10 +666,10 @@ class Registration(models.Model):
                     vals["stage_id"]
                 )
                 if (
-                        not registration.stage_id.fold
-                        and next_stage.fold
-                        and next_stage.sequence > 1
-                        and registration.active
+                    not registration.stage_id.fold
+                    and next_stage.fold
+                    and next_stage.sequence > 1
+                    and registration.active
                 ):  # ending stage
                     if not registration.beneficiary_id:
                         raise UserError(
@@ -684,8 +686,8 @@ class Registration(models.Model):
                         )
 
                 if (
-                        registration.stage_id.sequence > next_stage.sequence
-                        and registration.beneficiary_id
+                    registration.stage_id.sequence > next_stage.sequence
+                    and registration.beneficiary_id
                 ):
                     raise UserError(
                         _(
@@ -715,21 +717,21 @@ class Registration(models.Model):
     def _track_subtype(self, init_values):
         record = self[0]
         if (
-                "beneficiary_id" in init_values
-                and record.beneficiary_id
-                and record.beneficiary_id.active
+            "beneficiary_id" in init_values
+            and record.beneficiary_id
+            and record.beneficiary_id.active
         ):
             return "openg2p_registration.mt_registration_registered"
         elif (
-                "stage_id" in init_values
-                and record.stage_id
-                and record.stage_id.sequence <= 1
+            "stage_id" in init_values
+            and record.stage_id
+            and record.stage_id.sequence <= 1
         ):
             return "openg2p_registration.mt_registration_new"
         elif (
-                "stage_id" in init_values
-                and record.stage_id
-                and record.stage_id.sequence > 1
+            "stage_id" in init_values
+            and record.stage_id
+            and record.stage_id.sequence > 1
         ):
             return "openg2p_registration.mt_registration_stage_changed"
         return super(Registration, self)._track_subtype(init_values)
@@ -755,12 +757,12 @@ class Registration(models.Model):
         self.ensure_one()
 
         if (
-                not self.duplicate_beneficiaries_ids
+            not self.duplicate_beneficiaries_ids
         ):  # last chance to make sure no duplicates
             self.ensure_unique(mode=MATCH_MODE_COMPREHENSIVE)
 
         if (
-                self.duplicate_beneficiaries_ids
+            self.duplicate_beneficiaries_ids
         ):  # TODO ability to force create if maanger... pass via context
             raise ValidationError(
                 _("Potential duplicates exists for this record and so can not be added")
