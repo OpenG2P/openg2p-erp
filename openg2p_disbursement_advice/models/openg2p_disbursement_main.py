872613c7247921a497da8014aba524ded5c69bb7
shani <ishanranasingh@gmail.com>
2021-09-14 13:09:16 +0530

    disbursement apis

--- openg2p_disbursement_advice/models/openg2p_disbursement_main.py
+++ openg2p_disbursement_advice/models/openg2p_disbursement_main.py
@@ -107,3 +107,16 @@ class DisbursementMain(models.Model):
                 }
             }
         self.generate_uuid()
+
+    def api_json(self):
+        return {
+            "name":self.acc_holder_name,
+            "bank account no.":self.name,
+            "amount":self.amount,
+            "paymentmode":self.payment_mode,
+            "currency":self.currency_id.name,
+            "startdate":self.date_start,
+            "enddate":self.date_end
+        }
+    
+    
